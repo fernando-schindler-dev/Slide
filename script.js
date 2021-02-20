033bc81d275b0d766eaedfd4a76ba70928fa64cc
Fernando Schindler <fernando.schindler.dev@gmail.com>
2021-02-19 23:09:18 -0300

    Adicionado responsividade ao projeto

--- script.js
+++ script.js
@@ -1,103 +1,113 @@
 class Slide {
-  constructor(slide, menuSlide) {
+  constructor(slide, slideNav) {
     this.slideContainer = document.querySelector(slide);
     this.slides = [...document.querySelector(slide).children];
-    this.menuSlides = [...document.querySelector(menuSlide).children];
-    this.larguraSlide = this.slides[0].offsetWidth;
-    this.slideAtual = 0;
+    this.slidesBtns = [...document.querySelector(slideNav).children];
+    this.slideWidth = this.slides[0].offsetWidth;
+    this.currentSlide = 0;
   }
 
-  slideMouseUp(evento) {
-    this.posicaoMouseUp = evento.clientX;
-    this.slideContainer.removeEventListener("mousemove", this.slideMouseMove);
+  slideMouseUp(event) {
+    this.positionMouseUp = event.clientX;
+    this.slideContainer.removeEventListener('mousemove', this.slideMouseMove);
 
-    this.calculoMouseMove = this.posicaoMouseDown - this.posicaoMouseUp;
+    this.totalMouseMove = this.positionMouseDown - this.positionMouseUp;
 
-    this.slideContainer.style.transition = "0.3s";
-    if (this.calculoMouseMove > 200) {
-      this.selecionarSlide(this.slideAtual + 1);
-    } else if (this.calculoMouseMove < -200) {
-      this.selecionarSlide(this.slideAtual - 1);
+    this.slideContainer.style.transition = '0.3s';
+    if (this.totalMouseMove > 200) {
+      this.selectSlide(this.currentSlide + 1);
+    } else if (this.totalMouseMove < -200) {
+      this.selectSlide(this.currentSlide - 1);
     } else {
-      this.selecionarSlide(this.slideAtual);
+      this.selectSlide(this.currentSlide);
     }
   }
 
-  slideMouseMove(evento) {
-    this.slideContainer.style.transition = "0s";
+  slideMouseMove(event) {
+    this.slideContainer.style.transition = '0s';
 
-    this.posicaoMouseMove = evento.clientX;
-    this.quantidadePxTransform =
-      this.posicaoMouseMove -
-      this.posicaoMouseDown +
-      this.distanciasSlides[this.slideAtual];
+    this.positionMouseMove = event.clientX;
+    this.totalPxTransform =
+      this.positionMouseMove -
+      this.positionMouseDown +
+      this.slidesDistances[this.currentSlide];
 
-    this.slideContainer.style.transform = `translateX(${this.quantidadePxTransform}px)`;
+    this.slideContainer.style.transform = `translateX(${this.totalPxTransform}px)`;
   }
 
-  slideMouseDown(evento) {
-    this.posicaoMouseDown = evento.clientX;
-    this.slideContainer.addEventListener("mousemove", this.slideMouseMove);
+  slideMouseDown(event) {
+    this.positionMouseDown = event.clientX;
+    this.slideContainer.addEventListener('mousemove', this.slideMouseMove);
   }
 
-  adicionarClasse(classe, indexBtn) {
-    this.slides.forEach((item) => item.classList.remove(classe));
-    this.menuSlides.forEach((item) => item.classList.remove(classe));
-    this.slides[indexBtn].classList.add(classe);
-    this.menuSlides[indexBtn].classList.add(classe);
+  addClassName(className, btnIndex) {
+    this.slides.forEach((item) => item.classList.remove(className));
+    this.slidesBtns.forEach((item) => item.classList.remove(className));
+    this.slides[btnIndex].classList.add(className);
+    this.slidesBtns[btnIndex].classList.add(className);
   }
 
-  selecionarSlide(indexBtn) {
-    this.slideAtual = indexBtn;
+  selectSlide(btnIndex) {
+    this.currentSlide = btnIndex;
 
-    if (this.slideAtual < 0) {
-      this.slideAtual = 0;
-    } else if (this.slideAtual > this.slides.length - 1) {
-      this.slideAtual = this.slides.length - 1;
+    if (this.currentSlide < 0) {
+      this.currentSlide = 0;
+    } else if (this.currentSlide > this.slides.length - 1) {
+      this.currentSlide = this.slides.length - 1;
     }
 
-    this.adicionarClasse("ativo", this.slideAtual);
+    this.addClassName('active', this.currentSlide);
     this.slideContainer.style.transform = `translateX(${
-      this.distanciasSlides[this.slideAtual]
+      this.slidesDistances[this.currentSlide]
     }px)`;
   }
 
-  pegarDistanciasSlides() {
-    this.distanciasSlides = {};
+  getSlidesDistances() {
+    this.slidesDistances = {};
     this.slides.forEach((item, index) => {
-      this.distanciasSlides[index] = (item.offsetLeft - this.marginSlide) * -1;
+      this.slidesDistances[index] = (item.offsetLeft - this.slideMargin) * -1;
     });
   }
 
-  calcularMarginSlide() {
-    this.marginSlide = (window.innerWidth - this.larguraSlide) / 2;
+  calculateSlideMargin() {
+    this.slideMargin = (window.innerWidth - this.slideWidth) / 2;
   }
 
-  adicionarEventos() {
-    this.menuSlides.forEach((btnSlide, indexBtn) => {
-      btnSlide.addEventListener("click", () => this.selecionarSlide(indexBtn));
+  handleRezise() {
+    setTimeout(() => {
+      this.slideWidth = this.slides[0].offsetWidth;
+      this.calculateSlideMargin();
+      this.getSlidesDistances();
+      this.selectSlide(this.currentSlide);
+    }, 300);
+  }
+
+  addEvents() {
+    this.slidesBtns.forEach((btnSlide, btnIndex) => {
+      btnSlide.addEventListener('click', () => this.selectSlide(btnIndex));
     });
 
-    window.addEventListener("resize", this.calcularMarginSlide);
+    window.addEventListener('resize', this.handleRezise);
 
-    this.slideContainer.addEventListener("mousedown", this.slideMouseDown);
-    this.slideContainer.addEventListener("mouseup", this.slideMouseUp);
+    this.slideContainer.addEventListener('mousedown', this.slideMouseDown);
+    this.slideContainer.addEventListener('mouseup', this.slideMouseUp);
   }
 
-  bindEventos() {
+  bindEvents() {
     this.slideMouseDown = this.slideMouseDown.bind(this);
     this.slideMouseMove = this.slideMouseMove.bind(this);
     this.slideMouseUp = this.slideMouseUp.bind(this);
+    this.handleRezise = this.handleRezise.bind(this);
   }
 
-  iniciar() {
-    this.bindEventos();
-    this.adicionarEventos();
-    this.calcularMarginSlide();
-    this.pegarDistanciasSlides();
+  init() {
+    this.bindEvents();
+    this.addEvents();
+    this.calculateSlideMargin();
+    this.getSlidesDistances();
     return this;
   }
 }
 
-const slide = new Slide(".slide", ".menuSlide");
-slide.iniciar().selecionarSlide(0);
+const slide = new Slide('.slide', '.slideNav');
+slide.init().selectSlide(0);
